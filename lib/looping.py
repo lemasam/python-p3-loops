225cc340dbcdb4f5558e6b6b3b3d1af2b6415287
lema samuel <lemasam10@gmail.com>
2024-05-22 05:44:31 +0300

    Completed Assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    for i in range (10,0,-1):
+        print(i)
+        print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+    
+list1 = [1, 2, 3, 4, 5]
+list2 = [-1, -2, -3, -4, -5]
+
+result1 = square_integers(list1)
+result2 = square_integers(list2)
+
+print("Squared integers for list1:", result1)
+print("Squared integers for list2:", result2)
+pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range (1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
     pass
